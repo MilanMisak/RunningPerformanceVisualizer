ee9b2225ebb2fa81fc9cb3430e0cbbecc1fbdcc1
Milan Misak <admin@milanmisak.com>
2019-02-28 22:10:17 +0000

    Add a local athlete profile file for development

--- server/app.py
+++ server/app.py
@@ -4,14 +4,23 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/athlete/<id>')
-def get_athlete_data(id):
+def fetch_athlete_data(id):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'
     }
     r = requests.get(f'https://www.thepowerof10.info/athletes/profile.aspx?athleteid={id}', headers=headers)
     if r.status_code != 200:
         abort(r.status_code)
+    return r.text
 
-    soup = BeautifulSoup(r.text, 'html.parser')
+
+def load_debug_athlete_data():
+    with open('AthleteProfile.html', 'r') as f:
+        return f.read()
+
+
+@app.route('/athlete/<id>')
+def get_athlete_data(id):
+    html = load_debug_athlete_data() if app.config['DEBUG'] else fetch_athlete_data(id)
+    soup = BeautifulSoup(html, 'html.parser')
     return soup.h2.get_text()
