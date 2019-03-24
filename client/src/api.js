8a79f1f57998cccc896f22defc58b21feaa697f0
Milan Misak <admin@milanmisak.com>
2019-03-24 18:12:55 +0000

    Fix server URL in the client for development

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import {SERVER_URL} from 'config';
 
 export const getAthleteData = async id => {
-	const response = await fetch(`${SERVER_URL}/api/athlete/${id}`, {mode: 'cors'});
+	const response = await fetch(`${SERVER_URL}/athlete/${id}`, {mode: 'cors'});
 	return await response.json();
 };
