84d4fa11d4c25e25cbb34942a0fe6d8666e846a2
Milan Misak <admin@milanmisak.com>
2019-04-13 14:03:03 +0100

    Turn tabs into spaces in the webpack config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,60 +1,60 @@
 const path = require('path'),
-	HtmlWebpackPlugin = require('html-webpack-plugin'),
-	MiniCssExtractPlugin = require('mini-css-extract-plugin'),
-	TerserJSPlugin = require('terser-webpack-plugin'),
-	OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+    TerserJSPlugin = require('terser-webpack-plugin'),
+    OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = (env, argv) => {
-	const mode = argv.mode || 'development';
-	return {
-		output: {
-			publicPath: mode === 'production' ? './' : '/',
-			filename: '[name].[chunkhash].js',
-			chunkFilename: '[name].[chunkhash].js'
-		},
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					exclude: /node_modules/,
-					use: {
-						loader: 'babel-loader',
-					}
-				},
-				{
-					test: /\.scss$/,
-					use: [
-						mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-						'css-loader',
-						'sass-loader'
-					]
-				}
-			]
-		},
-		resolve: {
-			alias: {
-				config: path.join(__dirname, 'config', mode)
-			}
-		},
-		plugins: [
-			new HtmlWebpackPlugin({
-				template: 'src/index.html'
-			}),
-			new MiniCssExtractPlugin({
-				filename: '[name].[contenthash].css'
-			}),
-			new OptimizeCSSAssetsPlugin({
-				cssProcessorPluginOptions: {
-					preset: ['default', {discardComments: {removeAll: true}}],
-				}
-			})
-		],
-		optimization: mode === 'production'
-			? {
-				minimizer: [
-					new TerserJSPlugin()
-				]
-			}
-			: {}
-	};
+    const mode = argv.mode || 'development';
+    return {
+        output: {
+            publicPath: mode === 'production' ? './' : '/',
+            filename: '[name].[chunkhash].js',
+            chunkFilename: '[name].[chunkhash].js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            alias: {
+                config: path.join(__dirname, 'config', mode)
+            }
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'src/index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash].css'
+            }),
+            new OptimizeCSSAssetsPlugin({
+                cssProcessorPluginOptions: {
+                    preset: ['default', {discardComments: {removeAll: true}}],
+                }
+            })
+        ],
+        optimization: mode === 'production'
+            ? {
+                minimizer: [
+                    new TerserJSPlugin()
+                ]
+            }
+            : {}
+    };
 };
